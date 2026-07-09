9c34a38802ec03094647588abd496fa1adbea4ec
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:22:25 -0700

    Create 816-DesignHashset

---
+++ 816-DesignHashset/816-DesignHashset.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:52:34 AM
+class MyHashSet {
+    private boolean[] set;
+
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+
+    public void add(int key) {
+        set[key] = true;
+    }
+
+    public void remove(int key) {
+        set[key] = false;
+    }
+
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
