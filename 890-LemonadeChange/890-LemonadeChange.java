ee1e9eeb17c3fd008156286d95c69d61f29aa1ef
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:22:08 -0700

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.java
@@ -0,0 +1,36 @@
+// Last updated: 7/9/2026, 9:52:16 AM
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        for (int bill:bills){
+            switch(bill){
+                case 5:
+                five++;
+                break;
+
+                case 10:
+                if(five==0){
+                    return false;
+                }
+                five--;
+                ten++;
+                break;
+
+                case 20:
+                if(ten>=1 && five>=1){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else{
+                    return false;
+                }
+                break;
+            }
+        }
+        return true;
+    }
+}
