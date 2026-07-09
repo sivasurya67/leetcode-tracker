fbe23a5dc27afb37ef8ed61a74dda7f7ee634e3d
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:23:46 -0700

    Create 160-IntersectionOfTwoLinkedLists

---
+++ 160-IntersectionOfTwoLinkedLists/160-IntersectionOfTwoLinkedLists.java
@@ -0,0 +1,15 @@
+// Last updated: 7/9/2026, 9:53:54 AM
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+
+        ListNode pA = headA, pB = headB;
+
+        while (pA != pB) {
+            pA = (pA == null) ? headB : pA.next;
+            pB = (pB == null) ? headA : pB.next;
+        }
+
+        return pA; // Either intersection node or null
+    }
+}
