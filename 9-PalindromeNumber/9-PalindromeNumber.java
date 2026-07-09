76a668ea7283c61fa387d605ac4a2138af8553ee
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:24:59 -0700

    Create 9-PalindromeNumber

---
+++ 9-PalindromeNumber/9-PalindromeNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:55:07 AM
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int reverse = 0;
+        int xcopy = x;
+
+        while (x > 0) {
+            reverse = (reverse * 10) + (x % 10);
+            x /= 10;
+        }
+
+        return reverse == xcopy;        
+    }
+}
