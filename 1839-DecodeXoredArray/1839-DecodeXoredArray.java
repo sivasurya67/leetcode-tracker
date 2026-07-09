6aa422277c71683dbfdf4acf2a8649d27439a8ce
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:19:54 -0700

    Create 1839-DecodeXoredArray

---
+++ 1839-DecodeXoredArray/1839-DecodeXoredArray.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:50:02 AM
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int res[]=new int[encoded.length+1];
+        res[0]=first;
+        for(int i=0; i<encoded.length; i++){
+            res[i+1] = res[i]^encoded[i];
+        }
+        return res;
+    }
+}
