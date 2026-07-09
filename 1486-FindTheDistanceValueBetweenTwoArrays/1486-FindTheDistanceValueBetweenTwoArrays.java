e96e6c4e8568cc30bbeea820d9a20133f2fe61b9
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:20:31 -0700

    Create 1486-FindTheDistanceValueBetweenTwoArrays

---
+++ 1486-FindTheDistanceValueBetweenTwoArrays/1486-FindTheDistanceValueBetweenTwoArrays.java
@@ -0,0 +1,23 @@
+// Last updated: 7/9/2026, 9:50:39 AM
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+        int count = 0;
+
+        for (int num1 : arr1) {
+            boolean valid = true;
+
+            for (int num2 : arr2) {
+                if (Math.abs(num1 - num2) <= d) {
+                    valid = false;
+                    break;
+                }
+            }
+
+            if (valid) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
