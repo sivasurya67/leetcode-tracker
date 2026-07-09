dd4ec33bda91bff2e5ed617118854aac0ca647b6
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:22:04 -0700

    Create 919-ProjectionAreaOf3dShapes

---
+++ 919-ProjectionAreaOf3dShapes/919-ProjectionAreaOf3dShapes.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:52:12 AM
+class Solution {
+    public int projectionArea(int[][] grid) {
+        int xy = 0, row[] = new int[grid.length], col[] = new int[grid[0].length];
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] != 0)
+                    xy++;
+                if (grid[i][j] > row[i])
+                    row[i] = grid[i][j];
+                if (grid[i][j] > col[j])
+                    col[j] = grid[i][j];
+            }
+        int total = xy;
+        for (int i : row)
+            total += i;
+        for (int i : col)
+            total += i;
+        return total;
+    }
+}
