d7506430018727293aed556dc7e965988ce51255
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:23:20 -0700

    Create 303-RangeSumQueryImmutable

---
+++ 303-RangeSumQueryImmutable/303-RangeSumQueryImmutable.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:53:28 AM
+class NumArray {
+
+    private int[] prefixSum;
+
+    public NumArray(int[] nums) {
+        int n = nums.length;
+        prefixSum = new int[n + 1];
+        
+        prefixSum[0] = 0;
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+}
