0537b1e86a280d9008553731a3f9676c5d6c79f2
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:24:39 -0700

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,31 @@
+// Last updated: 7/9/2026, 9:54:47 AM
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k = 0; 
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; 
+                k++; 
+            }
+        }
+        return k;
+    }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums1 = {3, 2, 2, 3};
+        int val1 = 3;
+        int k1 = solution.removeElement(nums1, val1);
+        System.out.println(k1); 
+        for (int i = 0; i < k1; i++) {
+            System.out.print(nums1[i] + " "); 
+        }
+        System.out.println();
+        int[] nums2 = {0, 1, 2, 2, 3, 0, 4, 2};
+        int val2 = 2;
+        int k2 = solution.removeElement(nums2, val2);
+        System.out.println(k2);
+        for (int i = 0; i < k2; i++) {
+            System.out.print(nums2[i] + " "); 
+        }
+    }
+}
