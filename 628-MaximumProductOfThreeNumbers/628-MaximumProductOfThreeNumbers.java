ba9ff31a1bec26c3d20618e43ed6da7a769434bc
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:22:54 -0700

    Create 628-MaximumProductOfThreeNumbers

---
+++ 628-MaximumProductOfThreeNumbers/628-MaximumProductOfThreeNumbers.java
@@ -0,0 +1,11 @@
+// Last updated: 7/9/2026, 9:53:02 AM
+class Solution {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        
+        int n = nums.length;
+
+        return Math.max(nums[n - 1] * nums[n - 2] * nums[n - 3],
+                        nums[0] * nums[1] * nums[n - 1]);
+    }
+}
