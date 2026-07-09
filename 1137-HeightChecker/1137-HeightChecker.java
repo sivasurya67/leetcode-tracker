d0a0cb52c174e1d57483bc87a6479cc6ad04314b
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:21:27 -0700

    Create 1137-HeightChecker

---
+++ 1137-HeightChecker/1137-HeightChecker.java
@@ -0,0 +1,13 @@
+// Last updated: 7/9/2026, 9:51:35 AM
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+        int count = 0;
+        for (int i = 0; i < heights.length; i++) {
+            if (heights[i] != expected[i])
+                count++;
+        }
+        return count;
+    }
+}
