c4acde00621fca84bcec3de2eeb599fe22c1bb9e
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:19:19 -0700

    Create 2316-CountHillsAndValleysInAnArray

---
+++ 2316-CountHillsAndValleysInAnArray/2316-CountHillsAndValleysInAnArray.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 9:49:27 AM
+class Solution 
+{
+    public int countHillValley(int[] nums) 
+    {
+        List<Integer> filtered = new ArrayList<>();
+        filtered.add(nums[0]);
+        for (int i = 1; i < nums.length; i++) 
+        {
+            if (nums[i] != nums[i - 1]) 
+            {
+                filtered.add(nums[i]);
+            }
+        }
+        int count = 0;
+        for (int i = 1; i < filtered.size() - 1; i++) 
+        {
+            int prev = filtered.get(i - 1);
+            int curr = filtered.get(i);
+            int next = filtered.get(i + 1);
+            if ((curr > prev && curr > next) || (curr < prev && curr < next)) 
+            {
+                count++; 
+            }
+        }
+        return count;
+    }
+}
