aa49ca9f30a51a18ab293e0a9a01f3b44abd62fb
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:20:18 -0700

    Create 1532-ReformatTheString

---
+++ 1532-ReformatTheString/1532-ReformatTheString.java
@@ -0,0 +1,39 @@
+// Last updated: 7/9/2026, 9:50:26 AM
+class Solution {
+    public String reformat(String s) {
+        List<Character> letters = new ArrayList<>();
+        List<Character> digits = new ArrayList<>();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digits.add(c);
+            } else {
+                letters.add(c);
+            }
+        }
+
+        if (Math.abs(letters.size() - digits.size()) > 1) {
+            return "";
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        boolean letterFirst = letters.size() > digits.size();
+
+        int i = 0, j = 0;
+
+        while (i < letters.size() || j < digits.size()) {
+            if (letterFirst && i < letters.size()) {
+                result.append(letters.get(i++));
+            }
+            if (j < digits.size()) {
+                result.append(digits.get(j++));
+            }
+            if (!letterFirst && i < letters.size()) {
+                result.append(letters.get(i++));
+            }
+        }
+
+        return result.toString();
+    }
+}
