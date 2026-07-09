b17426208f1d79e962bb930bd1e21140ede8ad05
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:23:35 -0700

    Create 206-ReverseLinkedList

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:53:44 AM
+class Solution {
+    public ListNode reverseList(ListNode head) {
+    ListNode prev=null;
+    ListNode curr=head;
+    while(curr!=null){
+        ListNode  temp=curr.next;    //step1
+        curr.next=prev;              //step2
+         prev = curr;                //step3
+        curr=temp;                   //step4
+    }
+    return prev;
+    }
+}
