bf544446fa1ec9e798a5cfe0f501a0e64d7b3a27
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:21:54 -0700

    Create 950-XOfAKindInADeckOfCards

---
+++ 950-XOfAKindInADeckOfCards/950-XOfAKindInADeckOfCards.java
@@ -0,0 +1,29 @@
+// Last updated: 7/9/2026, 9:52:03 AM
+class Solution {
+    public boolean hasGroupsSizeX(int[] deck) {
+        
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0;i<deck.length;i++){
+            map.put(deck[i], map.getOrDefault(deck[i],0)+1);
+        }
+        
+        int ans=0;
+        
+        for(int key: map.keySet()){
+            
+            ans = gcd(ans, map.get(key));
+        }
+    
+        return ans>=2 ? true : false;
+        
+
+    }
+    
+    public int gcd(int a, int b){
+        if(b==0){
+            return a ;
+        }
+        return gcd(b, a%b);
+    }
+}
