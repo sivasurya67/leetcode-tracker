b8ccab0c7dff6673d95f8fd4b83025d8b7498e62
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:21:06 -0700

    Create 1298-ReverseSubstringsBetweenEachPairOfParentheses

---
+++ 1298-ReverseSubstringsBetweenEachPairOfParentheses/1298-ReverseSubstringsBetweenEachPairOfParentheses.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:51:14 AM
+class Solution {
+    public String reverseParentheses(String s) {
+        Deque<Integer> indStack = new LinkedList<>();
+        StringBuilder res = new StringBuilder();
+
+        for (char char_s : s.toCharArray()) {
+            if (char_s == '(') {
+                indStack.push(res.length());
+            } else if (char_s == ')') {
+                int startInd = indStack.pop();
+                String reversed = new StringBuilder(res.substring(startInd)).reverse().toString();
+                res.replace(startInd, res.length(), reversed);
+            } else {
+                res.append(char_s);
+            }
+        }
+
+        return res.toString();
+    }
+}
