46a0f8da174d0d90ca728072f2280935c4f894d6
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:20:04 -0700

    Create 1741-SortArrayByIncreasingFrequency

---
+++ 1741-SortArrayByIncreasingFrequency/1741-SortArrayByIncreasingFrequency.java
@@ -0,0 +1,30 @@
+// Last updated: 7/9/2026, 9:50:12 AM
+class Solution {
+        public int[] frequencySort(int[] nums) {
+                int[] result = new int[nums.length];
+                        HashMap<Integer, Integer> hm = new HashMap<>();
+                                for (int i : nums) {
+                                            hm.put(i, hm.getOrDefault(i, 0) + 1);
+                                                    }
+                                                            List<Map.Entry<Integer, Integer>> list = new ArrayList<>(hm.entrySet());
+                                                                    Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>(){
+                                                                               public int compare(Map.Entry<Integer,Integer> e1, Map.Entry<Integer,Integer> e2) {
+                                                                                              if (e1.getValue() == e2.getValue())
+                                                                                                                 return e2.getKey() - e1.getKey();
+                                                                                                                                else 
+                                                                                                                                                   return e1.getValue() - e2.getValue();
+                                                                                                                                                              } 
+                                                                                                                                                                      });
+                                                                                                                                                                              
+                                                                                                                                                                                      int i = 0;
+                                                                                                                                                                                              for (Map.Entry<Integer, Integer> k : list) {
+                                                                                                                                                                                                          int val = k.getValue();
+                                                                                                                                                                                                                      while(val > 0) {
+                                                                                                                                                                                                                                      result[i] = k.getKey();
+                                                                                                                                                                                                                                                      i++;
+                                                                                                                                                                                                                                                                      val--;
+                                                                                                                                                                                                                                                                                  }
+                                                                                                                                                                                                                                                                                          }
+                                                                                                                                                                                                                                                                                                  return result;
+                                                                                                                                                                                                                                                                                                      }
+                                                                                                                                                                                                                                                                                                      }
