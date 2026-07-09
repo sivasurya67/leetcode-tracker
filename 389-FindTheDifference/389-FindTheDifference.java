9374feeaa2aa2f764ba1e453a6e15e65f4eb410c
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:23:11 -0700

    Create 389-FindTheDifference

---
+++ 389-FindTheDifference/389-FindTheDifference.java
@@ -0,0 +1,9 @@
+// Last updated: 7/9/2026, 9:53:20 AM
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char c = 0;
+        for(char cs : s.toCharArray()) c ^= cs;
+        for(char ct : t.toCharArray()) c ^= ct;
+        return c;
+    }
+}
