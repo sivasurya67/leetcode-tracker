de6d7f1508c4047b5146a9337f29f5e6bcfa4446
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:20:50 -0700

    Create 1426-FindNUniqueIntegersSumUpToZero

---
+++ 1426-FindNUniqueIntegersSumUpToZero/1426-FindNUniqueIntegersSumUpToZero.java
@@ -0,0 +1,19 @@
+// Last updated: 7/9/2026, 9:50:58 AM
+class Solution {
+    public int[] sumZero(int n) {
+        int[] result = new int[n];
+
+        int index = 0;
+
+        for (int i = 1; i <= n / 2; i++) {
+            result[index++] = i;
+            result[index++] = -i;
+        }
+
+        if (n % 2 == 1) {
+            result[index] = 0;
+        }
+
+        return result;
+    }
+}
