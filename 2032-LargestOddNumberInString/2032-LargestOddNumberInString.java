44c9df22dce88415d27bc8ba772b8c8607a09ea6
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-07-08 21:19:27 -0700

    Create 2032-LargestOddNumberInString

---
+++ 2032-LargestOddNumberInString/2032-LargestOddNumberInString.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:49:35 AM
+class Solution {
+    public String largestOddNumber(String num) {
+        for (int i = num.length() - 1; i >= 0; i--) {
+            char ch = num.charAt(i);
+
+            if ((ch - '0') % 2 == 1) {
+                return num.substring(0, i + 1);
+            }
+        }
+
+        return "";
+    }
+}
