ec0ff9efae3ff43861215cdef678981284611295
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 10:01:06 +0530

    Create 830-PositionsOfLargeGroups

---
+++ 830-PositionsOfLargeGroups/830-PositionsOfLargeGroups.java
@@ -0,0 +1,17 @@
+// Last updated: 9/6/2026, 9:31:05 PM
+class Solution {
+    public List<List<Integer>> largeGroupPositions(String S) {
+        List<List<Integer>> ans = new ArrayList();
+        int i = 0, N = S.length(); // i is the start of each group
+        for (int j = 0; j < N; ++j) {
+            if (j == N-1 || S.charAt(j) != S.charAt(j+1)) {
+                // Here, [i, j] represents a group.
+                if (j-i+1 >= 3)
+                    ans.add(Arrays.asList(new Integer[]{i, j}));
+                i = j + 1;
+            }
+        }
+
+        return ans;
+    }
+}
