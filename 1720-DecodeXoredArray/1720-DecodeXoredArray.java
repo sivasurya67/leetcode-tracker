24c70e7bc32c4e166f2ad500912c98be123f96ea
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 09:53:57 +0530

    Create 1720-DecodeXoredArray

---
+++ 1720-DecodeXoredArray/1720-DecodeXoredArray.java
@@ -0,0 +1,11 @@
+// Last updated: 9/6/2026, 9:23:57 PM
+class Solution {
+    public int[] decode(int[] encoded, int first) {
+        int res[]=new int[encoded.length+1];
+        res[0]=first;
+        for(int i=0; i<encoded.length; i++){
+            res[i+1] = res[i]^encoded[i];
+        }
+        return res;
+    }
+}
