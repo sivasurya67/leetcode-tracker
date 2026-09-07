96ade882a5cd6c9a04df1d60c2310ed213d83d50
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 09:59:58 +0530

    Create 925-LongPressedName

---
+++ 925-LongPressedName/925-LongPressedName.java
@@ -0,0 +1,16 @@
+// Last updated: 9/6/2026, 9:29:58 PM
+class Solution {
+    public boolean isLongPressedName(String name, String typed) { 
+        int i = 0, n = name.length(), m = typed.length();
+
+        for (int j = 0; j < m; ++j) {
+            if (i < n && name.charAt(i) == typed.charAt(j)) {
+                ++i;
+            } else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1)) {
+                return false;
+            }
+        }
+
+        return i == n;
+    }
+}
