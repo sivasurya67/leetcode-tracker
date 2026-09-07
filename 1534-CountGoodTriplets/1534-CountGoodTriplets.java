06d5e69d65842e866e5c9550864f4c445d8a2e38
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 09:54:35 +0530

    Create 1534-CountGoodTriplets

---
+++ 1534-CountGoodTriplets/1534-CountGoodTriplets.java
@@ -0,0 +1,19 @@
+// Last updated: 9/6/2026, 9:24:35 PM
+class Solution {
+    public int countGoodTriplets(int[] arr, int a, int b, int c) {
+        int good = 0;
+        int n = arr.length;
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (Math.abs(arr[i] - arr[j]) <= a &&
+                        Math.abs(arr[j] - arr[k]) <= b &&
+                        Math.abs(arr[i] - arr[k]) <= c) {
+                        good++;
+                    }
+                }
+            }
+        }
+        return good;
+    }
+}
