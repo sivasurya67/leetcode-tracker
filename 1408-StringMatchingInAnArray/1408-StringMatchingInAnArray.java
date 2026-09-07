fc8f4c03497777c42e7b785190b948f0cf39604e
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 09:55:32 +0530

    Create 1408-StringMatchingInAnArray

---
+++ 1408-StringMatchingInAnArray/1408-StringMatchingInAnArray.java
@@ -0,0 +1,17 @@
+// Last updated: 9/6/2026, 9:25:31 PM
+class Solution {
+    public List<String> stringMatching(String[] words) {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = 0; j < words.length; j++) {
+                if (i != j && words[j].contains(words[i])) {
+                    result.add(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
