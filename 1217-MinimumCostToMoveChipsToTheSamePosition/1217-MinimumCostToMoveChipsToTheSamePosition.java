9823acf7238e19ef439070706f89d67db1f56991
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 09:57:51 +0530

    Create 1217-MinimumCostToMoveChipsToTheSamePosition

---
+++ 1217-MinimumCostToMoveChipsToTheSamePosition/1217-MinimumCostToMoveChipsToTheSamePosition.java
@@ -0,0 +1,17 @@
+// Last updated: 9/6/2026, 9:27:51 PM
+class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int even = 0;
+        int odd = 0;
+
+        for (int p : position) {
+            if (p % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+        }
+
+        return Math.min(even, odd);
+    }
+}
