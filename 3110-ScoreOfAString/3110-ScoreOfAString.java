d805f1e9241ac9854771796b496c5eefbd40a6d3
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 09:51:39 +0530

    Create 3110-ScoreOfAString

---
+++ 3110-ScoreOfAString/3110-ScoreOfAString.java
@@ -0,0 +1,10 @@
+// Last updated: 9/6/2026, 9:21:38 PM
+public class Solution {
+    public int scoreOfString(String s) {
+        int score = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            score += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return score;
+    }
+}
