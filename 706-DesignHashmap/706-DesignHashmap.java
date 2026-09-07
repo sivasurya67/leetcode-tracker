8bb132f94d4b5aa9951796eb791f9fd840782369
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 10:01:35 +0530

    Create 706-DesignHashmap

---
+++ 706-DesignHashmap/706-DesignHashmap.java
@@ -0,0 +1,17 @@
+// Last updated: 9/6/2026, 9:31:35 PM
+class MyHashMap {
+    int[] data;
+    public MyHashMap() {
+        data = new int[1000001];
+        Arrays.fill(data, -1);
+    }
+    public void put(int key, int val) {
+        data[key] = val;
+    }
+    public int get(int key) {
+        return data[key];
+    }
+    public void remove(int key) {
+        data[key] = -1;
+    }
+}
