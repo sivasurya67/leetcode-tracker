2ebc63108ac7d55b0a03c8c938dd517079068950
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-07 09:58:07 +0530

    Create 1154-DayOfTheYear

---
+++ 1154-DayOfTheYear/1154-DayOfTheYear.java
@@ -0,0 +1,28 @@
+// Last updated: 9/6/2026, 9:28:07 PM
+class Solution {
+    public int dayOfYear(String date) {
+        String[] parts = date.split("-");
+        
+        int year = Integer.parseInt(parts[0]);
+        int month = Integer.parseInt(parts[1]);
+        int day = Integer.parseInt(parts[2]);
+
+        int[] daysInMonth = {
+            31, 28, 31, 30, 31, 30,
+            31, 31, 30, 31, 30, 31
+        };
+
+        // Leap year check
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+            daysInMonth[1] = 29;
+        }
+
+        int result = day;
+
+        for (int i = 0; i < month - 1; i++) {
+            result += daysInMonth[i];
+        }
+
+        return result;
+    }
+}
