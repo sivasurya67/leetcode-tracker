d97b215bb3e0d924167af01c313fa9fedde3676b
SIVA SURYA M S <sivasuryams77@gmail.com>
2026-09-16 16:09:42 +0530

    Create 3754-ConcatenateNonZeroDigitsAndMultiplyBySumI

---
+++ 3754-ConcatenateNonZeroDigitsAndMultiplyBySumI/3754-ConcatenateNonZeroDigitsAndMultiplyBySumI.java
@@ -0,0 +1,12 @@
+// Last updated: 9/16/2026, 3:39:41 AM
+class Solution {
+    public long sumAndMultiply(int n) {
+        long x = 0, s = 0;
+        for (char c : String.valueOf(n).toCharArray())
+            if (c != '0') {
+                x = x * 10 + c - '0';
+                s += c - '0';
+            }
+        return x * s;
+    }
+}
